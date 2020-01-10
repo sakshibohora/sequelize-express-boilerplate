ec18433ca58ae4c654c622623b61c512fc649692
Sakshi Bohora <sakshi.bohora@bacancytechnology.com>
2020-01-10 18:22:35 +0530

    static dialect

--- dbCreate.js
+++ dbCreate.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENVIRONMENT === 'prod') {
 //     dialect: 'postgres',
 //   },
 // );
-const sequelize = new Sequelize(process.env.DB_DIALECT, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize('postgres', process.env.DB_USER, process.env.DB_PASS, {
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
 });
