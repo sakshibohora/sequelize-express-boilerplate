7476e2a8ea0ae885db42589ff7e0efff499ab52f
Sakshi Bohora <sakshi.bohora@bacancytechnology.com>
2020-01-10 16:16:44 +0530

    config change

--- src/config/config.js
+++ src/config/config.js
@@ -16,6 +16,7 @@ const x = {
     dialect: process.env.DB_DIALECT,
   },
   prod: {
+    user_env_variable: 'DATABASE_URL',
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
