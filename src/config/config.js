2466e422db48bf2de7988f2127b9c645ce842e94
Sakshi Bohora <sakshi.bohora@bacancytechnology.com>
2020-01-10 18:24:35 +0530

    static dialect

--- src/config/config.js
+++ src/config/config.js
@@ -22,7 +22,7 @@ const x = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'postgres',
   },
   stag: {
     username: process.env.DB_USER,
